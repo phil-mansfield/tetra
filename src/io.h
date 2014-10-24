b9f034c8f656d6447ee4030d8dba088af7b068be
Phil Mansfield <pmansfie@andrew.cmu.edu>
2014-10-24 00:53:03 -0500

    Cleaned up comments in io.h

--- src/io.h
+++ src/io.h
@@ -2,5 +2,6 @@
 #define TETRA_SRC_READ_DATA_H_
 
 struct gadget_header_t *read_gadget_header(char *file_name);
+struct gadget_particle_t *read_gadget_particles(char *file_name);
 
 #endif /* TETRA_SRC_READ_DATA_H_ */
