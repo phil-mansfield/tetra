500c643bb86abca5978d009a7c607c042db4a7c1
Phil Mansfield <pmansfie@andrew.cmu.edu>
2014-10-24 00:13:31 -0500

    Expanded the parameters that print_header prints out.

--- src/print_header.c
+++ src/print_header.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdint.h>
 
 #include "gadget_types.h"
 #include "io.h"
@@ -8,16 +9,23 @@ void print_header(char *file_name)
 {
     struct gadget_header_t *header = read_gadget_header(file_name);
 
-    printf("%20s %g\n", "Mass:", header->mass[1]);
+    printf("%20s: %u/%llu\n", "Particles", header->npart[1],
+           ((uint64_t*)header->npart_total)[0]);
+    printf("%20s: %g\n", "Mass", header->mass[1]);
+    printf("%20s: %g\n", "Redshift", header->redshift);
+    printf("%20s: %g\n", "Box Size", header->box_size);
+    printf("%20s: %g\n", "Omega M", header->omega_0);
+    printf("%20s: %g\n", "Omega L", header->omega_lambda);
+    printf("%20s: %g\n", "Hubble", header->hubble_param);
+    printf("%20s: %d\n", "Snapshot Files", header->num_files);
 
     free(header);
 }
 
 int main(int argc, char **argv)
 {
-    if (argc <= 1) {
-        fprintf(stderr, "%s requires target file names as input.\n", argv[0]);
-        return 1;
+    if(argc <= 1) {
+        fprintf(stderr, "%s requires input Gadget particle files.\n", argv[0]);
     }
 
     for (int i = 1; i < argc; i++) {
