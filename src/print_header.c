8c3495cfbe054d0402dfd7668b751b1f9ea70047
Phil Mansfield <pmansfie@andrew.cmu.edu>
2014-10-24 11:18:55 -0500

    Updated print_header to print out the first 20 particles, too.

--- src/print_header.c
+++ src/print_header.c
@@ -6,6 +6,13 @@
 #include "gadget_types.h"
 #include "io.h"
 
+void print_particle(struct gadget_particle_t *p)
+{
+    printf("%12"PRIu64" (%g, %g, %g) (%g, %g, %g)\n", p->id,
+           p->pos[0], p->pos[1], p->pos[2],
+           p->vel[0], p->vel[1], p->vel[2]);
+}
+
 void print_header(char *file_name)
 {
     struct gadget_header_t *header = read_gadget_header(file_name);
@@ -13,11 +20,15 @@ void print_header(char *file_name)
     uint64_t total = ((((uint64_t)header->npart_total[0]) << 32) +
                       header->npart_total[1]);
 
-    printf("%20s: %u/%"PRIu64"\n", "Particles", header->npart[1],total);
+    printf("%20s: %u/%"PRIu64"\n", "Particles", header->npart[1], total);
     printf("%20s: %g\n", "Mass", header->mass[1]);
     printf("%20s: %g\n", "Redshift", header->redshift);
     printf("%20s: %g\n", "Box Size", header->box_size);
     printf("%20s: %d\n", "Snapshot Files", header->num_files);
+
+    struct gadget_particle_t *ps = read_gadget_particles(file_name);
+
+    for (int i = 0; i < 20; i++) print_particle(ps + i);
 
     free(header);
 }
